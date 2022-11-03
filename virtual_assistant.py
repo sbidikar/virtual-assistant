0d75387d60101ae23f3b97e04eb8773840c97782
Siddharth Bidikar <siddharthbidikar@Siddharths-MBP.lan>
2022-11-03 19:43:26 -0400

    added commands

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -1,24 +1,108 @@
 import speech_recognition as sr # recognise speech
-import playsound
-from gtts import gTTS
-from time import ctime
+import playsound # to play an audio files
+from gtts import gTTS # google text to speech
+import random
+from time import ctime # get time details
+import webbrowser # open browser
+import time
+import os # to remove 
+import bs4 as bs
 
-r = sr.Recognizer()
 
+
+def there_exists(terms):
+    for term in terms:
+        if term in voice_data:
+            return True
+
+def engine_speak(text):
+    text = str(text)
+
+r = sr.Recognizer() # initialise a recogniser
 # listen for audio and convert it to text:
 def record_audio(ask=""):
-    with sr.Microphone() as source:
+    with sr.Microphone() as source: # microphone as source
         if ask:
             engine_speak(ask)
         audio = r.listen(source, 5, 5)  # listen for the audio via source
         print("Done Listening")
-        
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)  # convert audio to text
+        except sr.UnknownValueError: # error: recognizer does not understand
+            engine_speak('I did not get that')
+        except sr.RequestError:
+            engine_speak('Sorry, the service is down') # error: recognizer is not connected
+        print(">>", voice_data.lower()) # print what user said
+        return voice_data.lower()
+
 # get string and make a audio file to be played
 def engine_speak(audio_string):
     audio_string = str(audio_string)
+    tts = gTTS(text=audio_string, lang='en') # text to speech(voice)
+    r = random.randint(1,20000000)
+    audio_file = 'audio' + str(r) + '.mp3'
+    tts.save(audio_file) # save as mp3
+    playsound.playsound(audio_file) # play the audio file
+    os.remove(audio_file) # remove audio file
+
+def respond(voice_data):
+    # 1: greeting
+    if there_exists(['hey','hi','hello']):
+        greetings = ["hey, how can I help you", "hey, what's up?", "I'm listening", "how can I help you?", "hello"]
+        greet = greetings[random.randint(0,len(greetings)-1)]
+        engine_speak(greet)
+
+    # 2: name
+    if there_exists(["what is your name","what's your name","tell me your name"]):
+            engine_speak("My name is David")
+
+    # 3: greeting
+    if there_exists(["how are you","how are you doing"]):
+        engine_speak("I'm very well, thanks for asking ")
+
+    # 4: time
+    if there_exists(["what"] and ["time"]):
+        time = ctime().split(" ")[3].split(":")[0:2]
+        if time[0] == "00":
+            hours = '12'
+        else:
+            hours = time[0]
+        minutes = time[1]
+        time = hours + " hours and " + minutes + "minutes"
+        engine_speak(time)
+
+    # 5: search google
+    if there_exists(["search for"]) and 'youtube' not in voice_data:
+        search_term = voice_data.split("for")[-1]
+        url = "https://google.com/search?q=" + search_term
+        webbrowser.get().open(url)
+        engine_speak("Here is what I found for" + search_term + "on google")
+
+    # 6: search youtube
+    if there_exists(["youtube"]):
+        search_term = voice_data.split("for")[-1]
+        url = "https://www.youtube.com/results?search_query=" + search_term
+        webbrowser.get().open(url)
+        engine_speak("Here is what I found for " + search_term + "on youtube")
+
+    #7: get stock price
+    if there_exists(["price of"]):
+        search_term = voice_data.split("for")[-1]
+        url = "https://google.com/search?q=" + search_term
+        webbrowser.get().open(url)
+        engine_speak("Here is what I found for " + search_term + " on google")
     
-        
+    if there_exists(["exit", "quit", "goodbye"]):
+        engine_speak("GoodByee")
+        exit()
+
+
+time.sleep(1)
+
+
 while(1):
-    voice_data = record_audio("Recording") # get the voice input
+    voice_data = record_audio("") # get the voice input
     print("Done")
     print("Q:", voice_data)
+    respond(voice_data) # respond
